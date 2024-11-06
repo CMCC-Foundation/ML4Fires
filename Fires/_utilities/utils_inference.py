9b5944c69b628b73eadb0bd09b5328fd318c2e47
Reza Shatery <r.shatery@gmail.com>
2024-11-06 08:47:55 +0000

    Add notebook and functions for demo

---
+++ Fires/_utilities/utils_inference.py
@@ -0,0 +1,186 @@
+import numpy as np
+import xarray as xr
+import os
+import joblib
+import torch
+
+from Fires._datasets.torch_dataset import FireDataset
+from Fires._macros.macros import (
+    DRIVERS,
+    TARGETS,
+    MAX_HECTARES_100KM,
+)
+from Fires._plots.plot_utils import plot_dataset_map
+from Fires._scalers.standard import StandardScaler
+
+def load_input_data(data_path, time_start, time_end):
+    drivers, targets = DRIVERS, TARGETS
+    print(drivers, "\n", targets)
+
+    # open the dataset and choose a subset
+    dataset = xr.open_zarr(data_path)[drivers + targets].load()
+
+    test_data = dataset.sel(time=slice(time_start, time_end))
+
+    # load the land sea mask and substitute zeros with NaN values
+    lsm = test_data.lsm.mean(dim='time', skipna=True).values
+    lsm[lsm == 0] = np.nan
+    print(lsm.shape)
+
+    # define MAX_HECT_LSM_MAP as global
+    global MAX_HECT_LSM_MAP, lats, lons
+    lats = dataset.latitude.values
+    lons = dataset.longitude.values
+    MAX_HECT_LSM_MAP = lsm * MAX_HECTARES_100KM
+
+    return test_data
+
+def create_data_loader(data_path, run_id):
+    # define scaler
+    local_path = os.path.join(os.getcwd(), 'MLFLOW', f"{run_id}/scaler/scaler.dump")
+    x_scaler = joblib.load(local_path)
+
+    # define torch dataset
+    drivers, targets = DRIVERS, TARGETS
+    torch_dataset = FireDataset(
+        src=data_path,
+        drivers=drivers,
+        targets=targets,
+        years=list(range(2019,2021)),
+        scalers=[x_scaler, None]
+    )
+
+    torch_data_loader = torch.utils.data.DataLoader(
+        torch_dataset,
+        batch_size=1,
+        shuffle=True,
+        drop_last=True
+    )
+    
+    return torch_data_loader
+
+def compute_aggregated_data(data, other_data=None, operation="mean") -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+	"""
+	Compute the mean or difference between data, and aggregate along latitudes and longitudes
+
+	Parameters
+	----------
+	data : numpy.ndarray
+	 	Input data, can be unscaled or already scaled and masked depending on the operation to be performed
+	other_data : numpy.ndarray, optional
+	 	Optional input data for calculating the difference, also assumed to be scaled and masked
+		Required if `operation` is 'diff'.
+	operation : str
+		Operation to perform ("mean" for mean, "diff" for difference)
+
+	Returns
+	-------
+	tuple of np.ndarray
+		A tuple containing:
+			- data : np.ndarray
+				Scaled and masked data after the operation.
+			- descaled_on_lats : np.ndarray
+				Mean of data along latitudes.
+			- descaled_on_lons : np.ndarray
+				Mean of data along longitudes.
+	
+	Raises
+	------
+	ValueError
+		If `operation` is 'diff' and `other_data` is not provided.
+
+	"""
+
+	# define function name
+
+	data = data.copy()
+
+	if operation == "diff":
+		if other_data is None:
+			raise ValueError("other_data must be provided when operation is 'diff'")
+		# difference between data that has been masked and rescaled to the original size
+		data -= other_data
+	else:
+		# mask data with the land sea mask and rescale to original size
+		data *= MAX_HECT_LSM_MAP
+
+	descaled_on_lats = np.nanmean(data, axis=1)
+	descaled_on_lons = np.nanmean(data, axis=0)
+
+	print(f" {operation.capitalize()} of data: {data.shape}")
+	print(f" Max: {round(np.nanmax(data), 2)} \t Min: {round(np.nanmin(data), 2)}")
+	print(f" Lats Max: {round(np.nanmax(descaled_on_lats), 2)} \t Lons Max: {round(np.nanmax(descaled_on_lons), 2)}")
+
+	return data, descaled_on_lats, descaled_on_lons
+
+
+def up_and_lower_bounds(avg_value, std_value):
+	"""
+	Compute upper and lower bound values.
+
+	Parameters
+	----------
+	avg_value : np.ndarray or float
+		The average values.
+	std_value : np.ndarray or float
+		The standard deviation values.
+
+	Returns
+	-------
+	tuple
+		A tuple containing the upper bound and lower bound values.
+
+	"""
+
+	_upper = avg_value + std_value
+	_lower = avg_value - std_value
+	return _upper, _lower
+
+
+def process_and_plot_data(data, label, lats, lons, model_name):
+	"""
+	Process the data and generate plots.
+
+	Parameters
+	----------
+	data : xarray.DataArray or np.ndarray
+		Data to process; can be an xarray.DataArray for real data or a numpy.ndarray for predictions.
+	label : str
+		Label to use in the plot title.
+	lats : np.ndarray
+		Array of latitudes.
+	lons : np.ndarray
+		Array of longitudes.
+	model_name : str
+		Name of the model, used in the plot title.
+
+	"""
+	
+	# Verify data type and compute mean and standard deviation along time axis
+	if isinstance(data, xr.DataArray):
+		avg_on_time = data.mean(dim='time', skipna=True).data
+		std_on_time = data.std(dim='time', skipna=True).data
+		print(f"Is DataArray - AVG: {avg_on_time.shape} STD: {std_on_time.shape}")
+	else:
+		avg_on_time = np.nanmean(data, axis=0)[0, ...]
+		std_on_time = np.nanstd(data, axis=0)[0, ...]
+		print(f"NOT DataArray - AVG: {avg_on_time.shape} STD: {std_on_time.shape}")
+
+	# Aggregate data
+	avg_descaled, avg_on_lats, _ = compute_aggregated_data(data=avg_on_time)
+	_, std_on_lats, _ = compute_aggregated_data(data=std_on_time)
+
+	# Compute upper and lower boundaries
+	upperbound, lowerbound = up_and_lower_bounds(avg_value=avg_on_lats, std_value=std_on_lats)
+
+	# Plot data
+	plot_dataset_map(
+		avg_target_data=avg_descaled,
+		avg_data_on_lats=avg_on_lats,
+		lowerbound_data=lowerbound,
+		upperbound_data=upperbound,
+		lats=lats,
+		lons=lons,
+		title=f'{label} ({model_name.upper()})',
+		cmap='nipy_spectral_r'
+	)
