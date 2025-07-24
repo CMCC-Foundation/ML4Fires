5cf65f5e710a37155b6e8bb28934d79c95ce1aee
ShazAlvi <shahbaz.nihalalvi@gmail.com>
2025-07-24 12:31:13 +0200

    SA: Code and the notebook for batch_prediction

--- batch_prediction.py
+++ batch_prediction.py
@@ -1,11 +1,12 @@
 import xarray as xr
-
+import torch
 import warnings
 warnings.filterwarnings("ignore")
 
 from Fires._utilities.utils_inference import do_inference
 
 def get_prediction_for_data(dataset: xr.Dataset,
-                            model):
+                            model_path):
+    model = torch.load(model_path)
     return do_inference(dataset,
                         model)
