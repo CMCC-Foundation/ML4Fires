7613be9cefbf62d69cd624b3621d93ba035a983a
ShazAlvi <shahbaz.nihalalvi@gmail.com>
2025-07-24 12:50:12 +0200

    code and notebook for batch prediction

--- batch_prediction.py
+++ batch_prediction.py
@@ -4,9 +4,14 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from Fires._utilities.utils_inference import do_inference
+from Fires._utilities.utils_mlflow import load_model_from_local_path
+
 
 def get_prediction_for_data(dataset: xr.Dataset,
-                            model_path):
-    model = torch.load(model_path)
-    return do_inference(dataset,
-                        model)
+                            model_path,
+                           verbose=False):
+    model = load_model_from_local_path(path=model_path)
+    if verbose:
+        print(model)
+    return do_inference(dataset=dataset,
+                        model=model)
