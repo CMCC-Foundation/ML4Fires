e21bfdbdeb2c599909f1aee235cfc3b03d7e73bd
ShazAlvi <shahbaz.nihalalvi@gmail.com>
2025-07-25 15:56:04 +0200

    SA: Use dataset path

--- batch_prediction.py
+++ batch_prediction.py
@@ -7,11 +7,11 @@ from Fires._utilities.utils_inference import do_inference
 from Fires._utilities.utils_mlflow import load_model_from_local_path
 
 
-def get_prediction_for_data(dataset: xr.Dataset,
+def get_prediction_for_data(dataset_path: str,
                             model_path,
                            verbose=False):
     model = load_model_from_local_path(path=model_path)
     if verbose:
         print(model)
-    return do_inference(dataset=dataset,
+    return do_inference(dataset=xr.open_dataset(dataset_path),
                         model=model)
